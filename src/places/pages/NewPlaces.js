57342217b334310fc2f049530ecc61cc8c344284
GUILAVOGUI <sayonguilavogui518@yahoo.com>
2021-09-21 22:04:03 +0800

    first commit

---
+++ src/places/pages/NewPlaces.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewPlaces = () => {
+    return (
+        <div>
+            NewPlaces
+        </div>
+    )
+}
+
+export default NewPlaces
---
+++ src/user/pages/Users.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import UsersList from '../components/UsersList'
+
+const Users = () => {
+    const USERS = [
+        {
+            id: 'u1',
+            name: 'Sayon',
+            image: 'https://saguishop.com/assets/images/sg-443x472.png',
+            places: 3,
+        }
+    ]
+    return (
+        <UsersList items={USERS} />
+    )
+}
+
+export default Users
